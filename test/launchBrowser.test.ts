fb61198c0c342d7e45d0ad529134a92c24ab1840
ALTOM-PC63\raluca.popa <raluca.popa@altom.com>
2022-11-08 20:24:58 +0200

    launch multiple browsers and channel option

--- test/launchBrowser.test.ts
+++ test/launchBrowser.test.ts
@@ -1,16 +1,46 @@
-import { chromium } from "playwright";
+import { chromium, firefox, webkit } from "playwright";
 
 const baseURL = 'https://the-internet.herokuapp.com/';
 
-describe('Launch Browser', () => {
+describe('Launch Browsers', () => {
 
-    test('Open web page', async () => {
-        const browser = await chromium.launch({headless: false});
+    test('Open web page with Chromium', async () => {
+        const browser = await chromium.launch({ headless: false, slowMo: 3000});
         const context = await browser.newContext({
             recordVideo: {
                 dir: "videos/"
               }
             });
+        const page = await context.newPage();
+        await page.goto(baseURL);
+
+        await context.close();
+        await browser.close();
+    });
+
+    test('Open web page with Chromium: Microsoft Edge channel', async () => {
+        const browser = await chromium.launch({ headless: false, channel: "msedge", slowMo: 3000 });
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto(baseURL);
+
+        await context.close();
+        await browser.close();
+    });
+
+    test('Open web page with Firefox', async () => {
+        const browser = await firefox.launch({ headless: false, slowMo: 3000 });
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto(baseURL);
+
+        await context.close();
+        await browser.close();
+    });
+
+    test('Open web page with Webkit', async () => {
+        const browser = await webkit.launch({ headless: false, slowMo: 3000 });
+        const context = await browser.newContext();
         const page = await context.newPage();
         await page.goto(baseURL);
 
