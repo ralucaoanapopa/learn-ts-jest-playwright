5b48741187daaf7bb9b6d7d327c69b437ad16c00
ALTOM-PC63\raluca.popa <raluca.popa@altom.com>
2022-11-21 17:44:38 +0200

    inventory tests with filtering

--- page/inventorySaucePage.ts
+++ page/inventorySaucePage.ts
@@ -9,6 +9,10 @@ export default class InventoryPage extends BasePage {
     readonly inventoryListClass = ".inventory_list";
     readonly selectSortClass = ".product_sort_container";
     readonly shoppingCartClass = ".shopping_cart_link";
+    readonly optionZAXpath = "xpath=//option[@value='za']";
+    readonly itemLabelsClass = ".inventory_item_label";
+    readonly itemNamesClass = '.inventory_item_name';
+    readonly activeFilterClass = ".active_option";
 
     constructor(page: Page) {
         super(page);
@@ -18,28 +22,15 @@ export default class InventoryPage extends BasePage {
         await super.navigate(this.inventoryPage);
     }
 
+    // get methods
     public get burgerMenu() {
         if(this.page.locator(this.burgerMenuId))
             return this.page.locator(this.burgerMenuId);
         else throw new Error("Burger Menu button is not found");
     }
 
-    public async clickBurgerMenu() {
-        await this.burgerMenu.click();
-    }
-
     public get logoutLink() {
         return this.page.locator(this.logoutId);
-    }
-
-    public async clickLogoutLink() {
-        await this.logoutLink.click();
-    }
-
-    public async logoutUser() {
-        expect(await this.burgerMenu.isVisible()).toBe(true);
-        await this.clickBurgerMenu();
-        await this.clickLogoutLink();
     }
 
     public get productsList() {
@@ -52,5 +43,52 @@ export default class InventoryPage extends BasePage {
 
     public get shoppingCart() {
         return this.page.locator(this.shoppingCartClass);
+    }
+
+    public get optionZA() {
+        return this.page.locator(this.optionZAXpath);
+    }
+
+    public get itemLabelsList() {
+        return this.page.$$(this.itemLabelsClass);
+    }
+    public get itemNamesList() {
+        return this.page.$$(this.itemNamesClass);
+    }
+
+    public get activeFilterOption() {
+        return this.page.locator(this.activeFilterClass);
+    }
+
+    // async methods
+    public async clickBurgerMenu() {
+        await this.burgerMenu.click();
+    }
+
+    public async clickLogoutLink() {
+        await this.logoutLink.click();
+    }
+
+    public async logoutUser() {
+        expect(await this.burgerMenu.isVisible()).toBe(true);
+        await this.clickBurgerMenu();
+        await this.clickLogoutLink();
+    }
+
+    public async clickFilterNameDesc() {
+        expect(await this.optionZA.isHidden()).toBe(true);
+        await this.selectSortFilter.selectOption({ value: 'za'});
+    }
+
+    public async clickFilterPriceDesc() {
+        await this.selectSortFilter.selectOption({ value: 'hilo'});
+    }
+
+    public async clickFilterNameAsc() {
+        await this.selectSortFilter.selectOption({ value: 'az'});
+    }
+
+    public async clickFilterPriceAsc() {
+        await this.selectSortFilter.selectOption({ value: 'lohi'});
     }
 }
--- test/POM_saucedemo/inventory.test.ts
+++ test/POM_saucedemo/inventory.test.ts
@@ -41,6 +41,50 @@ describe('Test inventory page', () => {
         expect(await inventory.shoppingCart.isVisible()).toBe(true);
         expect(await inventory.productsList.isVisible()).toBe(true);
         expect(await inventory.selectSortFilter.isVisible()).toBe(true);
+
+        expect((await inventory.itemLabelsList).length).toBe(6);
+
+        expect(await inventory.activeFilterOption.innerText()).toBe(data.filterNameAsc);
+        let allProductNames = await inventory.itemNamesList;
+        expect(await allProductNames[0].innerText()).toBe(data.productBackpack);
+
+    });
+
+    test("Should be able to filter products by Name (Z -> A)", async () => {
+        expect(await inventory.activeFilterOption.innerText()).toBe(data.filterNameAsc);
+        
+        await inventory.clickFilterNameDesc();
+        expect(await inventory.activeFilterOption.innerText()).toBe(data.filterNameDesc);
+        let allProductNames = await inventory.itemNamesList;
+        expect(await allProductNames[0].innerText()).toBe(data.productAllThings);
+
+    });
+
+    test("Should be able to filter products by Price (High to Low)", async () => {
+        
+        await inventory.clickFilterPriceDesc();
+        expect(await inventory.activeFilterOption.innerText()).toBe(data.filterPriceDesc);
+        let allProductNames = await inventory.itemNamesList;
+        expect(await allProductNames[0].innerText()).toBe(data.productJacket);
+
+    });
+
+    test("Should be able to filter products by by Name (A -> Z)", async () => {
+        
+        await inventory.clickFilterNameAsc();
+        expect(await inventory.activeFilterOption.innerText()).toBe(data.filterNameAsc);
+        let allProductNames = await inventory.itemNamesList;
+        expect(await allProductNames[0].innerText()).toBe(data.productBackpack);
+
+    });
+
+    test("Should be able to filter products by Price (Low to High)", async () => {
+        
+        await inventory.clickFilterPriceAsc();
+        expect(await inventory.activeFilterOption.innerText()).toBe(data.filterPriceAsc);
+        let allProductNames = await inventory.itemNamesList;
+        expect(await allProductNames[0].innerText()).toBe(data.productOnesie);
+
     });
 
 });
